8ef3c1203d532a9df04aded1942b0cb854288d7d
Kevin Hallett <kevinmhallett@gmail.com>
2022-07-16 16:45:48 -0500

    refactor Environment to new file

--- src/main.ts
+++ src/main.ts
@@ -1,42 +1,7 @@
 import './styles/styles.css';
-import { CarRenderer } from './render';
-import { CarControls } from './controls';
-import { CarEntity } from './entities';
+import { Environment } from './environment';
 
 
-
-class Environment {
-  private carCanvas: HTMLCanvasElement = document.getElementById('car-canvas') as HTMLCanvasElement;
-  private ctx: CanvasRenderingContext2D;
-  private carRenderer: CarRenderer;
-  private carControls: CarControls = new CarControls();
-  private carEntity: CarEntity;
-  constructor() {
-    this.ctx = this.carCanvas.getContext('2d') as CanvasRenderingContext2D;
-    this.carEntity = new CarEntity(100, 100, 30, 50);
-    this.carRenderer = new CarRenderer(this.ctx, this.carEntity);
-  }
-
-  start = () => {
-    this.carControls.listen();
-    this.animate();
-  }
-
-  stop = () => {
-    this.carControls.close();
-  }
-
-  update = () => {
-    this.carEntity.update(this.carControls);
-  }
-
-  animate = () => {
-    this.update();
-    this.carCanvas.height = window.innerHeight;
-    this.carRenderer.render();
-    requestAnimationFrame(this.animate);
-  }
-}
 
 const env = new Environment();
 
